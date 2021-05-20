8e094df9333434a5b21e5df12e9d1499946e3c9c
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-20 14:31:25 +0200

    Created menu for data from user

--- src/pl/klaudia/library/io/DataReader.java
+++ src/pl/klaudia/library/io/DataReader.java
@@ -1,4 +1,31 @@
 package pl.klaudia.library.io;
 
+import pl.klaudia.library.model.Book;
+
+import java.util.Scanner;
+
 public class DataReader {
+    Scanner sc = new Scanner(System.in);
+
+    public void close(){
+        sc.close();
+    }
+    public Book readAndCreateBook(){
+        System.out.println("Title: ");
+        String title = sc.nextLine();
+        System.out.println("Author: ");
+        String author = sc.nextLine();
+        System.out.println("Publisher: ");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = sc.nextLine();
+        System.out.println("Release date: ");
+        int releaseDate = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Number of pages: ");
+        int pages = sc.nextInt();
+        sc.nextLine();
+
+        return new Book(title,author,releaseDate,pages,publisher,isbn);
+    }
 }
