2b524d996fe9369cd30c984df36f693595aa9565
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-20 14:18:10 +0200

    Board for new books in class Library

--- src/pl/klaudia/library/app/Library.java
+++ src/pl/klaudia/library/app/Library.java
@@ -5,11 +5,13 @@ import pl.klaudia.library.model.Book;
 public class Library {
     public static void main(String[] args) {
 
-        Book book = new Book("HP","SW",2007,500,"Greg","987654");
-        Book book1 = new Book("GH","SW",2007,500,"Greg");
+        Book[] books = new Book[1000];
 
-        book.printInfo();
-        book1.printInfo();
+       books[0] = new Book("HP","SW",2007,500,"Greg","987654");
+       books[1] = new Book("GH","SW",2007,500,"Greg");
+
+        books[0].printInfo();
+        books[1].printInfo();
 
     }
 }
