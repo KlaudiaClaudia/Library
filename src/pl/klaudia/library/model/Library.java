7e1c9488d0b5e3fbde78c1d121030f6b417b3b81
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-22 11:11:55 +0200

    Siplify Library class, added exception

--- src/pl/klaudia/library/model/Library.java
+++ src/pl/klaudia/library/model/Library.java
@@ -8,46 +8,20 @@ public class Library {
 
 
     public void addBook(Book book){
-        if (publicationNumber < MAX_PUBLICATIONS){
-            publications[publicationNumber] = book;
-            publicationNumber ++;
-        }else {
-            System.out.println("The maximum number of books has been reached");
-        }
+        addPublication(book);
     }
-    public void printBooks(){
-        int countBooks = 0;
-        for (int i = 0; i < publicationNumber; i++) {
-            if (publications[i]instanceof Book){
-                System.out.println(publications[i]);
-                countBooks++;
-            }
-        }
-        if (countBooks==0){
-            System.out.println("No books in the library");
-        }
-    }
+
     public void addMagazine(Magazine magazine) {
-        if(publicationNumber < MAX_PUBLICATIONS) {
-            publications[publicationNumber] = magazine;
-            publicationNumber++;
-        } else {
-            System.out.println("The maximum number of magazines has been reached");
-        }
-
+        addPublication(magazine);
     }
 
-    public void printMagazines() {
-        int countMagazines =0;
-        for (int i = 0; i <publicationNumber ; i++) {
-            if (publications[i]instanceof Magazine){
-                System.out.println(publications[i]);
-                countMagazines++;
-            }
+    private void addPublication(Publication publication) {
+        if (publicationNumber>= MAX_PUBLICATIONS){
+            throw new ArrayIndexOutOfBoundsException("Max publications exceeded " + MAX_PUBLICATIONS);
         }
-        if (countMagazines==0){
-            System.out.println("No magazines in the library");
-        }
+        publications[publicationNumber]= publication;
+        publicationNumber++;
     }
+
 
 }
