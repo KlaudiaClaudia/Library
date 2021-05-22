788a6f9674432789be3c4d0b7fdb59978fb87c05
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-22 11:52:25 +0200

    Option is now a private internal type in LibraryControl Private, because we do not use it anywhere else and no one except this class needs to know that such a type exists at all

--- src/pl/klaudia/library/app/LibraryControl.java
+++ src/pl/klaudia/library/app/LibraryControl.java
@@ -105,5 +105,31 @@ public class LibraryControl {
         dataReader.close();
     }
 
+    private enum Option{
+        EXIT(0, "Exit the program"),
+        ADD_BOOK(1, "Add new book"),
+        ADD_MAGAZINE(2,"Add new magazine"),
+        PRINT_BOOKS(3, "View available books"),
+        PRINT_MAGAZINES(4, "View available magazines");
+
+        private int value;
+        private String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+        @Override
+        public String toString() {
+            return value + "-" + description;
+        }
+        static Option createFromInt(int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            }catch (ArrayIndexOutOfBoundsException e){
+                throw new NoSuchOptionException("No option for id " + option);
+            }
+        }
+    }
 
 }
