866ecf9128392b36d9703d15ef538e4cd47e943a
KlaudiaClaudia <claudiadabpriv@gmail.com>
2021-05-26 20:21:39 +0200

    Added delete book and magazine option for user and method

--- src/pl/klaudia/library/app/LibraryControl.java
+++ src/pl/klaudia/library/app/LibraryControl.java
@@ -54,6 +54,12 @@ public class LibraryControl {
                 case PRINT_MAGAZINES:
                     printMagazines();
                     break;
+                case DELETE_BOOK:
+                    deleteBook();
+                    break;
+                case DELETE_MAGAZINE:
+                    deleteMagazine();
+                    break;
                 case EXIT:
                     exit();
                     break;
@@ -117,6 +123,29 @@ public class LibraryControl {
         Publication[] publications = library.getPublications();
         printer.printMagazines(publications);
     }
+    private void deleteMagazine() {
+        try {
+            Magazine magazine = dataReader.readAndCreateMagazine();
+            if (library.removePublication(magazine))
+                printer.printLine("UMagazine deleted.");
+            else
+                printer.printLine("No specified magazine.");
+        } catch (InputMismatchException e) {
+            printer.printLine("Magazine creation failed, invalid data");
+        }
+    }
+
+    private void deleteBook() {
+        try {
+            Book book = dataReader.readAndCreateBook();
+            if (library.removePublication(book))
+                printer.printLine("Book deleted.");
+            else
+                printer.printLine("No specified book.");
+        } catch (InputMismatchException e) {
+            printer.printLine("Book creation failed, invalid data");
+        }
+    }
 
     private void exit() {
         try {
@@ -134,7 +163,9 @@ public class LibraryControl {
         ADD_BOOK(1, "Add new book"),
         ADD_MAGAZINE(2,"Add new magazine"),
         PRINT_BOOKS(3, "View available books"),
-        PRINT_MAGAZINES(4, "View available magazines");
+        PRINT_MAGAZINES(4, "View available magazines"),
+        DELETE_BOOK(5, "Delete book"),
+        DELETE_MAGAZINE(6, "Delete magazine");
 
         private int value;
         private String description;
